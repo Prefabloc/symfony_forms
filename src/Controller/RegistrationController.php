4f90db02b1f53ef8196118c1fa1da20c3c1bac21
Vraën <christophehoarau14@gmail.com>
2024-05-15 09:31:48 +0200

    Retrait du setting role a User sur le register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -21,7 +21,6 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager , SocieteRepository $societeRepository): Response
     {
         $user = new User();
-        $user->setRoles(["ROLE_USER"]);
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
