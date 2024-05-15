19c0a48af40ab9b6c7f422e89e6692065b9f4bd4
PFB-Jeremy <jeremy.lebon@prefabloc.re>
2024-05-15 15:53:55 +0400

    resolve conflicts

--- src/Controller/LitigeController.php
+++ src/Controller/LitigeController.php
@@ -13,22 +13,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class LitigeController extends AbstractController
 {
-    #[Route('/litige', name: 'app_litige')]
-    public function index(Request $request, EntityManagerInterface $entityManager ): Response
+    #[Route('/litige', name: 'app_litige', methods: ['GET'])]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $url = $request->getUri();
 
         $form = $this->createForm(LitigeQualiteType::class);
 
         $litige = new LitigeQualite();
-        $litigeForm = $this->createForm( LitigeQualiteType::class , $litige ) ;
+        $litigeForm = $this->createForm(LitigeQualiteType::class, $litige);
         $litigeForm->handleRequest($request);
 
-        if ( $litigeForm->isSubmitted() && $litigeForm->isValid() ) {
+        if ($litigeForm->isSubmitted() && $litigeForm->isValid()) {
             $entityManager->persist($litige);
             $entityManager->flush();
 
-            $this->addFlash('success' , "Saisie du litige enregistrée !");
+            $this->addFlash('success', "Saisie du litige enregistrée !");
         }
 
         return $this->render('litige/index.html.twig', [
@@ -38,24 +38,4 @@ class LitigeController extends AbstractController
             'litigeForm' => $form->createView(),
         ]);
     }
-
-//    #[Route('/litige/saisie' , name : 'app_litige_saisie')]
-//    public function litigeSaisie(Request $request , EntityManagerInterface $entityManager ) : Response
-//    {
-//        $litige = new LitigeQualite();
-//        $litigeForm = $this->createForm( LitigeQualiteType::class , $litige ) ;
-//        $litigeForm->handleRequest($request);
-//
-//        if ( $litigeForm->isSubmitted() && $litigeForm->isValid() ) {
-//            $entityManager->persist($litige);
-//            $entityManager->flush();
-//
-//            $this->addFlash('success' , "Saisie du litige enregistrée !");
-//            return $this->redirectToRoute('app_litige_saisie');
-//        } else {
-//            return $this->render('litige/index.html.twig', [
-//                 'litigeForm' => $litigeForm->createView()
-//            ]);
-//        }
-//    }
 }
