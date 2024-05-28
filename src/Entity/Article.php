2765d1a4292e383a3db052649612333a63b03f60
Vraën <christophehoarau14@gmail.com>
2024-05-28 12:01:13 +0200

    Fix Entity Article, tout remarche

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,7 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
