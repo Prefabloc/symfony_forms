32ae546895b4501fa735010b333dc3a2ee983160
Amélie Ducasse <amel.ducasse@gmail.fr>
2024-05-30 16:25:04 +0200

    Modal Signalement : modif view + create Entity, Controller, Form

---
+++ src/Controller/SignalementController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Signalement;
+use App\Form\SignalementType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SignalementController extends AbstractController
+{
+    #[Route('/signalement', name: 'app_signalement')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $signalement = new Signalement();
+        $signalementForm = $this->createForm(SignalementType::class, $signalement);
+        $signalementForm->handleRequest($request);
+
+        if ($signalementForm->isSubmitted() && $signalementForm->isValid()) {
+            $entityManager->persist($signalement);
+            $entityManager->flush();
+            $this->addFlash('succes', "Saisie du signalement enregistré !");
+        }
+        return $this->render('includes/_modalSignalement.html.twig', [
+            'controller_name' => 'SignalementController',
+            'signalementForm' => $signalementForm->createView()
+        ]);
+    }
+}
