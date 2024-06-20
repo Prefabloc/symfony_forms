815ad5aabaf744f44dcfb663c6ac312d8aa740a6
Vraën <christophehoarau14@gmail.com>
2024-06-20 13:59:39 +0200

    Classe camera totalement opérationnelle

--- public/scripts/CameraClass.js
+++ public/scripts/CameraClass.js
@@ -1,7 +1,6 @@
 
 class CameraClass
 {
-
     constructor( boutonAfficherDivPhoto, boutonMasquerDivPhoto , divPhoto , divPhotoTablette , divModal , urlFetch ){
         this.boutonAfficherDivPhoto = boutonAfficherDivPhoto ;
         this.boutonMasquerDivPhoto = boutonMasquerDivPhoto;
@@ -14,7 +13,12 @@ class CameraClass
         this.height = window.innerHeight ;
         this.stream = null;
 
+        //variables bool qui servent pour la double vérification
+        this.boolPhoto = false ;
+        this.boolUpload = false ;
+        //formData atteignable depuis l'extérieur pour envoyer ce qu'on veut en plus de la photo, attention à gérer le controller après
         this.formData = new FormData();
+
 
         //div Photo
         this.video = document.createElement('video');
@@ -41,6 +45,7 @@ class CameraClass
         this.structureDivPhoto = this.structureDivPhoto.bind(this);
         this.structureDivPhotoTablette = this.structureDivPhotoTablette.bind(this);
         this.structureModalConfirm = this.structureModalConfirm.bind(this);
+        this.init = this.init.bind(this);
         this.showCamera = this.showCamera.bind(this);
         this.hideCamera = this.hideCamera.bind(this);
         this.startCamera = this.startCamera.bind(this);
@@ -48,13 +53,15 @@ class CameraClass
         this.capturePicture = this.capturePicture.bind(this);
         this.erasePicture = this.erasePicture.bind(this);
         this.uploadPhoto = this.uploadPhoto.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
 
         this.boutonAfficherDivPhoto.addEventListener( 'click' , () => this.showCamera() );
         this.boutonMasquerDivPhoto.addEventListener( 'click' , () => this.hideCamera() );
         this.boutonSnap.addEventListener('click' , () => this.capturePicture() );
         this.boutonEffacer.addEventListener( 'click' , () => this.erasePicture() );
         this.boutonEnvoyer.addEventListener( 'click', () => this.uploadPhoto() );
-
+        this.uploadFromDevice.addEventListener("click", () => this.uploadFile( this.inputFile ))
+        this.uploadFromDevice2.addEventListener("click", () => this.uploadFile( this.inputFile2 ))
     }
 
     //fonction pour remplir la divPhoto
@@ -83,6 +90,7 @@ class CameraClass
         //bouton de capture
         this.boutonSnap.style.display = 'none';
         this.boutonSnap.innerText = 'Prendre une photo';
+        this.boutonSnap.classList = "px-3 py-2 text-sm me-2 mb-2 font-medium text-center inline-flex items-center text-white bg-amber-800 rounded-lg hover:bg-blue-800 dark:bg-slate-500 dark:hover:bg-slate-700 "
         this.divBoutonSnap.append(this.boutonSnap);
 
         //div de l'upload
@@ -97,10 +105,12 @@ class CameraClass
         //input file
         this.inputFile.type = 'file';
         this.inputFile.style.height = '1.5rem';
+        this.inputFile.classList = "block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-black focus:outline-none dark:bg-orange-100 dark:border-black dark:placeholder-black"
         this.divUpload.append(this.inputFile);
 
         //bouton Upload from Device
         this.uploadFromDevice.innerText = 'Envoyer';
+        this.uploadFromDevice.classList = "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
         this.divUpload.append(this.uploadFromDevice);
 
         //On met tout dans la divPhoto
@@ -110,12 +120,15 @@ class CameraClass
     structureDivPhotoTablette (){
         //label
         this.labelInput.innerText = 'Upload file'
+        this.labelInput.classList = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 
         //input
         this.inputFile2.type = 'file';
+        this.inputFile2.classList = "block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-black focus:outline-none dark:bg-orange-100 dark:border-black dark:placeholder-black"
 
         //bouton d'envoi
         this.uploadFromDevice2.innerText = 'Envoyer'
+        this.uploadFromDevice2.classList = "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
 
         //liaison à la div
         this.divPhotoTablette.append( this.labelInput , this.inputFile2 , this.uploadFromDevice2 )
@@ -123,11 +136,12 @@ class CameraClass
 
     structureModalConfirm() {
         //style de la balise dialog qui va recevoir les autres divs
-        this.divModal.className = 'modaleConfirmation' ;
+        this.divModal.className = 'modalConfirmationPhotoIdentificationPrestation' ;
 
         //canvas qui recevra la photo
         this.canvas.width = 640 ;
         this.canvas.height = 480 ;
+        this.canvas.className = 'canvasPhoto'
 
         //paragraphe pour phrase confirmation
         this.para.innerText = 'Confirmez vous vouloir utiliser cette photo ?'
@@ -138,15 +152,23 @@ class CameraClass
         //bouton pour effacer le canvas
         this.boutonEffacer.className = 'boutonsConfirmPhoto';
         this.boutonEffacer.innerText = 'Effacer';
+        this.boutonEffacer.id = 'effacer'
         this.divBoutonsConfirm.append(this.boutonEffacer);
 
         //bouton pour envoyer la photo
         this.boutonEnvoyer.className = 'boutonsConfirmPhoto'
         this.boutonEnvoyer.innerText = 'Envoyer' ;
+        this.boutonEnvoyer.id = 'upload'
         this.divBoutonsConfirm.append(this.boutonEnvoyer);
 
         //On met tout dans la div Modal
         this.divModal.append( this.canvas , this.para , this.divBoutonsConfirm );
+    }
+
+    init()  {
+        this.structureDivPhoto();
+        this.structureDivPhotoTablette();
+        this.structureModalConfirm();
     }
 
     showCamera() {
@@ -165,8 +187,8 @@ class CameraClass
             this.divPhotoTablette.style.display = 'none';
         } else {
             this.divPhoto.style.display = 'none';
+            this.boutonMasquerDivPhoto.style.display = 'none';
             this.stopCamera();
-            this.boutonMasquerDivPhoto.display = 'none';
             this.boutonAfficherDivPhoto.style.display = 'flex';
         }
     }
@@ -226,7 +248,7 @@ class CameraClass
         this.divModal.style.display = 'none';
         this.divModal.close();
 
-        this.canvas.toBlob( function ( blob) {
+        this.canvas.toBlob(( blob) => {
             this.formData.append( 'photo' , blob , 'photo.png' );
 
             fetch( this.urlFetch  , {
@@ -235,12 +257,36 @@ class CameraClass
             })
                 .then( response => response.json() )
                 .then( data => {
+                    this.boolPhoto = true ;
                     alert( 'Photo envoyée avec succès' );
                 })
                 .catch( error => {
                     console.error( 'Erreur lors de l\'envoi de la photo : ' , error );
                 })
         })
+    }
+
+    uploadFile( fileInput ) {
+        const file = fileInput.files[0] ;
+
+        if ( file ) {
+            this.formData.append( 'photo2' , file );
+
+            fetch( this.urlFetch , {
+                method : 'POST',
+                body : this.formData
+            })
+                .then( response => response.json())
+                .then( data => {
+                    alert('Photo envoyée avec succès.');
+                    this.boolUpload = true ;
+                })
+                .catch( error => {
+                    console.error('Erreur lors de l\'envoi du fichier.' + error )
+                })
+        } else {
+            alert( 'Veuillez séléctionner un fichier.');
+        }
     }
 
 
